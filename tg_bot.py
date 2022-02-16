285e1dde082e0799b9de8e2b84b1d1b0290c4d19
GavriKON <98122857+GavriKON@users.noreply.github.com>
2022-02-16 20:34:08 +0200

    mini update

--- tg_bot.py
+++ tg_bot.py
@@ -15,7 +15,6 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands="all_news")
 async def get_all_news(message: types.Message):
     for i in range(len(box_for_info_text)-1, -1, -1):
-        await bot.send_photo(message.chat.id, images[i], parse_mode="HTML")
         await message.answer(f"<b>{b[i]}</b>" + box_for_info_text[i], parse_mode="HTML")
 if __name__ == "__main__":
     executor.start_polling(dp)
