f8198314dc3a329bfd4f4afc2c2f35648615562d
GavriKON <98122857+GavriKON@users.noreply.github.com>
2022-02-16 20:42:35 +0200

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -15,6 +15,6 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands="all_news")
 async def get_all_news(message: types.Message):
     for i in range(len(box_for_info_text)-1, -1, -1):
-        await message.answer(f"<b>{b[i]}</b>" + box_for_info_text[i] + print(len(box_for_info_text)), parse_mode="HTML")
+        await message.answer(f"<b>{b[i]}</b>" + box_for_info_text[i] + + f"{len(box_for_info_text)}, parse_mode="HTML")
 if __name__ == "__main__":
     executor.start_polling(dp)
